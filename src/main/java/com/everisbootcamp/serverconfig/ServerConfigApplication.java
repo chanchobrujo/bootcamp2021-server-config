c91c1344ed608f274d995de7867bf38c78fed324
kevinpalma20 <umb.kevsidorov@gmail.com>
2021-09-12 15:38:12 -0500

    Update config git

--- src/main/java/com/everisbootcamp/serverconfig/ServerConfigApplication.java
+++ src/main/java/com/everisbootcamp/serverconfig/ServerConfigApplication.java
@@ -2,12 +2,18 @@ package com.everisbootcamp.serverconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableConfigServer
 @SpringBootApplication
 public class ServerConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerConfigApplication.class, args);
+		log.info("SERVER CONFIG ENABLED");
 	}
 
 }
