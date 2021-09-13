5d5db73f7b502dd6dfa2daec6232a6a553740d90
kevinpalma20 <umb.kevsidorov@gmail.com>
2021-09-12 21:52:23 -0500

    Update adding routes

--- src/main/java/com/everisbootcamp/serverconfig/ServerConfigApplication.java
+++ src/main/java/com/everisbootcamp/serverconfig/ServerConfigApplication.java
@@ -9,8 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @EnableConfigServer
 @SpringBootApplication
-public class ServerConfigApplication {
-
+public class ServerConfigApplication { 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerConfigApplication.class, args);
 		log.info("SERVER CONFIG ENABLED");
